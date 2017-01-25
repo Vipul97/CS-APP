c46f5b976e5a9dca114fc4d45bb7387ca65bb1a4
Vipul Gharde <vipul.gharde@gmail.com>
2017-01-25 20:45:32 +0530

    Create 2.57.c

---
+++ Part I  Program Structure and Execution/2 Representing and Manipulating Information/2.57.c
@@ -0,0 +1,18 @@
+void show_bytes(byte_pointer start, size_t len) {
+    int i;
+    for (i = 0; i < len; i++)
+        printf(" %.2x", start[i]);
+    printf("\n");
+}
+
+void show_short(short x) {
+    show_bytes((byte_pointer) &x, sizeof(short));
+}
+
+void show_long(long x) {
+    show_bytes((byte_pointer) &x, sizeof(long));
+}
+
+void show_double(double x) {
+    show_bytes((byte_pointer) &x, sizeof(double));
+}
